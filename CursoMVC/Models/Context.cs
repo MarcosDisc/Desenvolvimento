56879d32160278f378d636d9a387046505043483
MarcosDisc <marcosdisc@gmail.com>
2021-02-15 10:38:28 -0300

    Primeiro commit...

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder
+                .UseMySQL("server=127.0.0.1;port=3306;database=cursoMVC;user=root;password=Oxford@0202");
+                //.UseLoggerFactory(LoggerFactory.Create(b => b.AddConsole().AddFilter(level => level >= LogLevel.Information)))
+                //.EnableSensitiveDataLogging()
+                //.EnableDetailedErrors();
+        }
+
+    }
+}
